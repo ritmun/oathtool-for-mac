ede85a367d513541561b1a544d54b82e70b638bb
Ritu Mundhe <rmundhe@redhat.com>
2021-07-17 17:12:26 -0500

    removed cd

--- tokgen.sh
+++ tokgen.sh
@@ -1,14 +1,13 @@
 #Todo: Replace <some phrase 1> and <some phrase 2> with your passphrases.
 
-cd ~/.oathtool
-gpg --batch --output tempt --passphrase <some phrase 1> --decrypt token.gpg
-gpg --batch --output tempq  --passphrase <some phrase 2>> --decrypt qrcode.gpg
+gpg --batch --output ~/.oathtool/tempt --passphrase <some phrase 1> --decrypt ~/.oathtool/token.gpg
+gpg --batch --output ~/.oathtool/tempq  --passphrase <some phrase 2>> --decrypt ~/.oathtool/qrcode.gpg
 
-echo "$(cat tempt)$(oathtool --base32 --totp $(<tempq))"  > tmp                      
-rm -f tempq.txt
-rm -f tempt.txt
-pbcopy < tmp
-rm -f tmp
+echo "$(cat ~/.oathtool/tempt)$(oathtool --base32 --totp $(<~/.oathtool/tempq))"  > ~/.oathtool/tmp                      
+rm -f ~/.oathtool/tempq.txt
+rm -f ~/.oathtool/tempt.txt
+pbcopy < ~/.oathtool/tmp
+rm -f ~/.oathtool/tmp
 echo "Token pbcopied. Hit enter to clear clipboard when it is pasted."
 read ip
 pbcopy < /dev/null
