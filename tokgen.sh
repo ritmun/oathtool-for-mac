3c0126b1c5c3c4d63642bfd8d14723921ebc9ac9
Ritu Mundhe <rmundhe@redhat.com>
2021-07-17 21:23:12 -0500

    Changing word 'token' to 'key'. Confusing.

--- tokgen.sh
+++ tokgen.sh
@@ -1,11 +1,12 @@
 #Todo: Replace <some phrase 1> and <some phrase 2> with your passphrases.
 
-gpg --batch --output ~/.oathtool/tempt --passphrase <some phrase 1> --decrypt ~/.oathtool/token.gpg
+gpg --batch --output ~/.oathtool/tempk --passphrase <some phrase 1> --decrypt ~/.oathtool/key.gpg
 gpg --batch --output ~/.oathtool/tempq  --passphrase <some phrase 2> --decrypt ~/.oathtool/qrcode.gpg
 
-echo "$(cat ~/.oathtool/tempt)$(oathtool --base32 --totp $(<~/.oathtool/tempq))"  > ~/.oathtool/tmp                      
+#Following should be adjusted for the token format expected in your organization's auth. Here, the format is key+totp
+echo "$(cat ~/.oathtool/tempk)$(oathtool --base32 --totp $(<~/.oathtool/tempq))"  > ~/.oathtool/tmp                      
 rm -f ~/.oathtool/tempq
-rm -f ~/.oathtool/tempt
+rm -f ~/.oathtool/tempk
 pbcopy < ~/.oathtool/tmp
 rm -f ~/.oathtool/tmp
 echo "Token pbcopied. Hit enter to clear clipboard when it is pasted."
