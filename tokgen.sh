a86ea159d989f00dbed7b777c18e42751a58a01c
Ritu Mundhe <rmundhe@redhat.com>
2021-07-17 21:07:51 -0500

    fixed filenames

--- tokgen.sh
+++ tokgen.sh
@@ -1,11 +1,11 @@
 #Todo: Replace <some phrase 1> and <some phrase 2> with your passphrases.
 
 gpg --batch --output ~/.oathtool/tempt --passphrase <some phrase 1> --decrypt ~/.oathtool/token.gpg
-gpg --batch --output ~/.oathtool/tempq  --passphrase <some phrase 2>> --decrypt ~/.oathtool/qrcode.gpg
+gpg --batch --output ~/.oathtool/tempq  --passphrase <some phrase 2> --decrypt ~/.oathtool/qrcode.gpg
 
 echo "$(cat ~/.oathtool/tempt)$(oathtool --base32 --totp $(<~/.oathtool/tempq))"  > ~/.oathtool/tmp                      
-rm -f ~/.oathtool/tempq.txt
-rm -f ~/.oathtool/tempt.txt
+rm -f ~/.oathtool/tempq
+rm -f ~/.oathtool/tempt
 pbcopy < ~/.oathtool/tmp
 rm -f ~/.oathtool/tmp
 echo "Token pbcopied. Hit enter to clear clipboard when it is pasted."
